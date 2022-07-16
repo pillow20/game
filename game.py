2cfacda731a5339e4c52e4d75a3eecc80f9f6eb2
alexey <fagj@mail.ru>
2022-07-16 10:19:22 +0300

    dfdfd

---
+++ game.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+import pygame, sys
+
+pygame.init()
+ 
+screen = pygame.display.set_mode((480,480))
+clock = pygame.time.Clock()
+
+
+bg_surface = pygame.image.load('icon.png')
+
+
+floor_surface= pygame.image.load('floor.png')
+ 
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    screen.blit(bg_surface,(0,0))  
+
+    screen.blit(floor_surface,(0,380))   
+
+
+
+    
+      
+            
+            
+    pygame.display.update()
+    clock.tick()        
+    
+    
+    
+    
+    
